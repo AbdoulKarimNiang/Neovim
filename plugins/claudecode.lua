4aad9b135220a2d9fb9d6db7b420b9fe65066e10
Abdoul Karim <karimniang9@gmail.com>
2026-03-30 14:59:46 +0200

    Power Query Syntax Highlight from main

---
+++ plugins/claudecode.lua
@@ -0,0 +1,13 @@
+-- lua/plugins/claudecode.lua
+return {
+  "coder/claudecode.nvim",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  config = true,
+  keys = {
+    { "<leader>ac", "<cmd>ClaudeCode<cr>",      desc = "Toggle Claude Code" },
+    { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude Code" },
+    { "<leader>as", "<cmd>ClaudeCodeSend<cr>",  mode = "v", desc = "Send selection to Claude" },
+  },
+}
